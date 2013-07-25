a23640bc57766acab563ecae490a23b265d76411
Andrew Nicols <andrew@nicols.co.uk>
2013-07-25 15:49:58 +0100

    local_ombieltoken: Initial commit - version 2012120300

---
+++ lang/en/local_ombieltoken.php
@@ -0,0 +1,3 @@
+<?php
+
+$string['pluginname'] = 'Ombiel token generator';
---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component  = 'local_ombieltoken';
+$plugin->version    = 2012120300;
+$plugin->requires   = 2012061700;
